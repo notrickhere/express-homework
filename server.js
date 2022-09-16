1ba3784a4f39be253c27dd3613037d3cf75f9b8b
Ricky Lee <rickylee150@gmail.com>
2022-09-15 21:51:33 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ app.get('/greeting', (req, res) => {
     res.send('<h1>Hello Stranger</h1>')
 })
 
-app.get('/greeting/:name', (req, res) => {
-    res.send(`Hello ${req.params.name}`)
+// app.get('/greeting/:name', (req, res) => {
+//     res.send(`Hello ${req.params.name}`)
+// })
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    res.send(`${req.params.total * (req.params.tipPercentage/100)}`)
 })
 
 app.listen(3000, () => {
